99b54ac610ee0661e54f38ba327808ee2f4d190d
Maxim Mityutko <maxim.mityutko@nike.com>
2024-08-30 19:15:15 +0200

    working server

--- src/koheesio/integrations/spark/tableau/server.py
+++ src/koheesio/integrations/spark/tableau/server.py
@@ -1,16 +1,15 @@
+import os
+from enum import Enum
+from pathlib import PurePath
+from typing import ContextManager
 from typing import Optional, Union
 
-import os
 import urllib3
-from abc import ABC, abstractmethod
-from pathlib import PurePath
+from pydantic import SecretStr, Field
 from tableauserverclient import PersonalAccessTokenAuth, TableauAuth, Server, ProjectItem, Pager, DatasourceItem
 
+from koheesio.models import model_validator
 from koheesio.steps import Step, StepOutput
-from koheesio.models import model_validator, field_validator
-
-from typing import ContextManager
-from pydantic import SecretStr, Field
 
 
 class TableauServer(Step):
@@ -58,21 +57,26 @@ class TableauServer(Step):
         description="ID of the project on the Tableau server",
     )
 
-    # @model_validator(mode="before")
-    # def validate_project(cls, data: dict) -> dict:
-    #     """Validate the path, make sure path and file_name are Path objects."""
-    #     project = data.get("project")
-    #     project_id = data.get("project_id")
-    #
-    #     if project and project_id:
-    #         raise ValueError("Both 'project' and 'project_id' parameters cannot be provided at the same time.")
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.server = None
 
-    def __authenticate(self) -> ContextManager:
+    @model_validator(mode="after")
+    def validate_project(cls, data: dict) -> dict:
+        """Validate when project and project_id are provided at the same time."""
+        project = data.get("project")
+        project_id = data.get("project_id")
+
+        if project and project_id:
+            raise ValueError("Both 'project' and 'project_id' parameters cannot be provided at the same time.")
+
+    @property
+    def auth(self) -> ContextManager:
         """
         Authenticate on the Tableau server.
 
         Example:
-            with self.__authenticate():
+            with self._authenticate():
 
         Returns:
             TableauAuth or PersonalAccessTokenAuth authorization object
@@ -90,18 +94,11 @@ class TableauServer(Step):
                 token_name=self.token_name, personal_access_token=self.token_value, site_id=self.site_id
             )
 
-        self.__server = Server(self.url)
-        self.__server.version = self.version
-        self.__server.add_http_options({"verify": False})
+        self.server = Server(self.url)
+        self.server.version = self.version
+        self.server.add_http_options({"verify": False})
 
-        with self.__server.auth.sign_in(tableau_auth):
-            # TODO: logging and check if authorized
-            if not self.site_id:
-                raise ValueError("Invalid credentials. Cannot create authorization to connect to Tableau Server.")
-
-        self.log.debug(f"Authorized in Tableau Server `{self.url}` and site `{self.site_id}`")
-
-        return self.__server.auth.sign_in(tableau_auth)
+        return self.server.auth.sign_in(tableau_auth)
 
     @property
     def working_project(self) -> Union[ProjectItem, None]:
@@ -110,6 +107,8 @@ class TableauServer(Step):
         The goal is to uniquely identify specific project on the server, if multiple projects have the same
         name, the `parent_project` attribute of the TableauServer is required.
 
+        Set `parent_project` value to 'root' if the project is located in the root directory.
+
         If `id` of the project is known, it can be used in `project_id` parameter, then the detection of the working
         project using the `project` and `parent_project` attributes is skipped.
 
@@ -117,24 +116,20 @@ class TableauServer(Step):
             ProjectItem: ProjectItem object representing the working project
         """
 
-        with self.__authenticate():
-            all_projects = Pager(self._server.projects)
+        with self.auth:
+            all_projects = Pager(self.server.projects)
             parent, lim_p = None, []
 
             for project in all_projects:
                 if project.id == self.project_id:
                     lim_p = [project]
-                    self.log.info(
-                        f"\nProject ID provided directly:\n\tName: {lim_p[0].name}\n\tID: {lim_p[0].id}"
-                    )
+                    self.log.info(f"\nProject ID provided directly:\n\tName: {lim_p[0].name}\n\tID: {lim_p[0].id}")
                     break
 
                 # Identify parent project
                 if project.name.strip() == self.parent_project and not self.project_id:
                     parent = project
-                    self.log.info(
-                        f"\nParent project identified:\n\tName: {parent.name}\n\tID: {parent.id}"
-                    )
+                    self.log.info(f"\nParent project identified:\n\tName: {parent.name}\n\tID: {parent.id}")
 
                 # Identify project(s)
                 if project.name.strip() == self.project and not self.project_id:
@@ -142,7 +137,7 @@ class TableauServer(Step):
 
             # Further filter the list of projects by parent project id
             if self.parent_project == "root" and not self.project_id:
-                lim_p = [p for p in lim_p if p.parent_id == 0]
+                lim_p = [p for p in lim_p if not p.parent_id]
             elif self.parent_project and parent and not self.project_id:
                 lim_p = [p for p in lim_p if p.parent_id == parent.id]
 
@@ -154,51 +149,48 @@ class TableauServer(Step):
             elif len(lim_p) == 0:
                 raise ValueError("Working project could not be identified.")
             else:
-                self.log.info(
-                    f"\nWorking project identified:\n\tName: {lim_p[0].name}\n\tID: {lim_p[0].id}"
-                )
+                self.log.info(f"\nWorking project identified:\n\tName: {lim_p[0].name}\n\tID: {lim_p[0].id}")
                 return lim_p[0]
 
     def execute(self):
         raise NotImplementedError("Method `execute` must be implemented in the subclass.")
 
 
+class TableauHyperPublishMode(str, Enum):
+    """
+    The different publishing modes for the TableauHyperPublisher.
+    """
+
+    APPEND = Server.PublishMode.Append
+    OVERWRITE = Server.PublishMode.Overwrite
+
+
 class TableauHyperPublisher(TableauServer):
     datasource_name: str = Field(default=..., description="Name of the datasource to publish")
     hyper_path: PurePath = Field(default=..., description="Path to Hyper file")
-    publish_mode: Server.PublishMode = Field(
-        default=Server.PublishMode.Overwrite,
+    publish_mode: TableauHyperPublishMode = Field(
+        default=TableauHyperPublishMode.OVERWRITE,
         description="Publish mode for the Hyper file",
     )
 
     class Output(StepOutput):
         """Output class for HyperFileListWriter"""
+
         datasource_item: DatasourceItem = Field(
-            default=...,
-            description="DatasourceItem object representing the published datasource"
+            default=..., description="DatasourceItem object representing the published datasource"
         )
-
-    @field_validator("publish_mode")
-    def validate_publish_mode(cls, value: str) -> str:
-        """
-        Only allow "Append" and "Overwrite" publish modes.
-        """
-        if value not in [Server.PublishMode.Append, Server.PublishMode.Overwrite]:
-            raise ValueError(f"Only {Server.PublishMode.Append} and {Server.PublishMode.Overwrite} are allowed")
-
-        return value
 
     def execute(self):
         # Ensure that the Hyper File exists
         if not os.path.isfile(self.hyper_path):
             raise FileNotFoundError(f"Hyper file not found at: {self.hyper_path.as_posix()}")
 
-        with self.__authenticate():
+        with self.auth:
             # Finally, publish the Hyper File to the Tableau server
             self.log.info(f'Publishing Hyper File located at: "{self.hyper_path.as_posix()}"')
             self.log.debug(f"Create mode: {self.publish_mode}")
 
-            datasource_item = self.__server.datasources.publish(
+            datasource_item = self.server.datasources.publish(
                 datasource_item=DatasourceItem(project_id=self.working_project.id, name=self.datasource_name),
                 file=self.hyper_path.as_posix(),
                 mode=self.publish_mode,
