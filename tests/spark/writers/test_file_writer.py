56e4f6c1897ea51418e440db0151871e100f7385
Mikita Sakalouski <38785549+mikita-sakalouski@users.noreply.github.com>
2024-10-23 02:10:26 +0200

    fix: test

--- tests/spark/writers/test_file_writer.py
+++ tests/spark/writers/test_file_writer.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from unittest.mock import MagicMock
 
-
-from koheesio.spark import DataFrame
 from koheesio.spark.writers import BatchOutputMode
 from koheesio.spark.writers.file_writer import FileFormat, FileWriter
 
@@ -22,7 +20,19 @@ def test_execute(dummy_df, mocker):
 
     mock_df_writer = MagicMock()
 
-    mocker.patch.object(DataFrame, "write", mock_df_writer)
+    from koheesio.spark.utils.connect import is_remote_session
+
+    if is_remote_session():
+        from pyspark.sql import DataFrame as SparkDataFrame
+        from pyspark.sql.connect.dataframe import DataFrame as ConnectDataFrame
+
+        mocker.patch.object(SparkDataFrame, "write", mock_df_writer)
+        mocker.patch.object(ConnectDataFrame, "write", mock_df_writer)
+    else:
+        from pyspark.sql import DataFrame
+
+        mocker.patch.object(DataFrame, "write", mock_df_writer)
+
     mock_df_writer.options.return_value = mock_df_writer
 
     writer.execute()
