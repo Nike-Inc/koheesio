4751375674cf0b9dd3b0aa2b6e717aab8c542bb8
Danny Meijer <10511979+dannymeijer@users.noreply.github.com>
2024-10-29 16:02:16 +0100

    version bump

--- src/koheesio/__about__.py
+++ src/koheesio/__about__.py
@@ -12,7 +12,7 @@ enhancing productivity and code maintainability.
 
 LICENSE_INFO = "Licensed as Apache 2.0"
 SOURCE = "https://github.com/Nike-Inc/koheesio"
-__version__ = "0.8.1"
+__version__ = "0.9.0rc0"
 __logo__ = (
     75,
     (
