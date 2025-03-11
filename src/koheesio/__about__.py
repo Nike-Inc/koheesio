6fb0f85c096d9fdba99c5b71fe8ee7c924fd0440
Danny Meijer <10511979+dannymeijer@users.noreply.github.com>
2025-03-11 14:52:27 +0100

    [DOCS] update release notes for version 0.10.1 and fix version number in __about__.py

--- src/koheesio/__about__.py
+++ src/koheesio/__about__.py
@@ -12,7 +12,7 @@ enhancing productivity and code maintainability.
 
 LICENSE_INFO = "Licensed as Apache 2.0"
 SOURCE = "https://github.com/Nike-Inc/koheesio"
-__version__ = "0.10.1a0"
+__version__ = "0.10.1"
 __logo__ = (
     75,
     (
