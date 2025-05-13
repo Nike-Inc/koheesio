e620c8b88710b295debfec9e4626483ae69faced
Mikita Sakalouski <38785549+mikita-sakalouski@users.noreply.github.com>
2025-05-13 23:47:25 +0200

    fix: enhance PySpark connect module imports for window functions

--- src/koheesio/spark/utils/common.py
+++ src/koheesio/spark/utils/common.py
@@ -137,8 +137,11 @@ if check_if_pyspark_connect_module_is_available():
         DataStreamReader,
         DataStreamWriter,
     )
+    from pyspark.sql.connect.window import Window as ConnectWindow
+    from pyspark.sql.connect.window import WindowSpec as ConnectWindowSpec
     from pyspark.sql.streaming.query import StreamingQuery
     from pyspark.sql.types import DataType as SqlDataType
+    from pyspark.sql.window import Window, WindowSpec
 
     Column = Union[sql.Column, ConnectColumn]
     DataFrame = Union[sql.DataFrame, ConnectDataFrame]
@@ -150,6 +153,8 @@ if check_if_pyspark_connect_module_is_available():
     DataFrameWriter = Union[sql.readwriter.DataFrameWriter, DataFrameWriter]  # type: ignore
     DataStreamWriter = Union[sql.streaming.readwriter.DataStreamWriter, DataStreamWriter]  # type: ignore
     StreamingQuery = StreamingQuery
+    Window = Union[Window, ConnectWindow]
+    WindowSpec = Union[WindowSpec, ConnectWindowSpec]
 else:
     """Import the regular PySpark modules if the current version of PySpark does not support the connect module"""
     try:
@@ -164,6 +169,7 @@ else:
     from pyspark.sql.readwriter import DataFrameReader, DataFrameWriter  # type: ignore
     from pyspark.sql.session import SparkSession  # type: ignore
     from pyspark.sql.types import DataType  # type: ignore
+    from pyspark.sql.window import Window, WindowSpec  # type: ignore
 
     try:
         from pyspark.sql.streaming.query import StreamingQuery  # type: ignore
@@ -179,6 +185,8 @@ else:
     DataFrameWriter = DataFrameWriter
     DataStreamWriter = DataStreamWriter
     StreamingQuery = StreamingQuery
+    Window = Window
+    WindowSpec = WindowSpec
 
 
 def get_active_session() -> SparkSession:  # type: ignore
