1ea018c9928857f986a44e2b7c9bfebd596f00f5
Mikita Sakalouski <38785549+mikita-sakalouski@users.noreply.github.com>
2025-09-08 13:11:58 +0200

    chore: set version to 0.11.0a0

--- src/koheesio/__about__.py
+++ src/koheesio/__about__.py
@@ -13,7 +13,7 @@ enhancing productivity and code maintainability.
 LICENSE_INFO = "Licensed as Apache 2.0"
 SOURCE = "https://github.com/Nike-Inc/koheesio"
 
-__version__ = "0.10.6"
+__version__ = "0.11.0a0"
 __logo__ = (
     75,
     (
